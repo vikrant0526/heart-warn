f3ce179423a6caa90f6159843dae42b8a36b98e4
vikrant0526 <60457380+vikrant0526@users.noreply.github.com>
2022-07-06 12:23:12 +0200

    Debug 2

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,11 @@
-import { SessionProvider } from "next-auth/react";
+import { SessionProvider, signOut } from "next-auth/react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider>
       <Component {...pageProps} />
+      <button onClick={signOut}>Sign out</button>
     </SessionProvider>
   );
 }
